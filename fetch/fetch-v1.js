56b2b32a350697b20afb1184075a0050b0ffa7af
6-Fercho-9 <fernandopadilla5840@gmail.com>
2025-07-25 23:58:16 -0400

    feat:agregando fetch

---
+++ fetch/fetch-v1.js
@@ -0,0 +1,20 @@
+const url = "https://jsonplaceholder.typicode.com/comments";
+async function obtenerDatosDeAPI(){
+    try {
+        const response = await fetch(url);
+        const res = await response.json();
+        console.log(res);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function obtenerDatav2 (url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((error) => {
+        console.error("Error al obtener los datos:", error?.message);
+    })
+}
+obtenerDatosDeAPI();
